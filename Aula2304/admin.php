68fa44f2151590f977af60b32399f7819394944b
Eridan <genealerooo@gmail.com>
2025-04-23 17:10:22 -0400

    Criacao tabelas

--- Aula2304/admin.php
+++ Aula2304/admin.php
@@ -1,1 +1,50 @@
 <?php
+session_start();
+include("conn.php");
+
+$query = "select * from usuarios";
+$result = mysqli_query( $con ,$query);
+
+?>
+<!-- $retorno = mysqli_fetch_all($result);
+// echo $retorno['nome'];
+// echo $retorno['senha'];
+// echo $retorno['setor']; -->
+
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Senha</th>
+                <th>Setor</th>
+            </tr>
+        </thead>
+        <?php while ($retorno = mysqli_fetch_array($result)){?>
+        <tbody>
+            <tr>
+                <td>
+                    <?php echo $retorno['nome'];?>
+                </td>
+                <td>
+                    <?php echo $retorno['senha'];?>
+                </td>
+                <td>
+                    <?php echo $retorno['setor'];?>
+                </td>
+            </tr>
+            <?php }?>
+        </tbody>
+    </table>
+</body>
+
+</html>
